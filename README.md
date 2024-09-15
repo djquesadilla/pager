894c56569ef0cb11ab404fa24642b4d25f090fe0
Fábio Costa <fabio@syntheticusers.com>
2024-09-15 15:33:24 +0100

    Initial commit with models and testing

---
+++ README.md
@@ -0,0 +1,44 @@
+# Use Cases
+
+These are the use cases we're going to implement in this project to test the Pager.
+
+```
+Given a Monitored Service in a Healthy State,
+when the Pager receives an Alert related to this Monitored Service,
+then the Monitored Service becomes Unhealthy,
+the Pager notifies all targets of the first level of the escalation policy,
+and sets a 15-minutes acknowledgement delay.
+```
+
+```
+Given a Monitored Service in an Unhealthy State,
+the corresponding Alert is not Acknowledged
+and the last level has not been notified,
+when the Pager receives the Acknowledgement Timeout,
+then the Pager notifies all targets of the next level of the escalation policy
+and sets a 15-minutes acknowledgement delay.
+```
+
+```
+Given a Monitored Service in an Unhealthy State
+when the Pager receives the Acknowledgement
+and later receives the Acknowledgement Timeout,
+then the Pager doesn't notify any Target
+and doesn't set an acknowledgement delay.
+```
+
+```
+Given a Monitored Service in an Unhealthy State,
+when the Pager receives an Alert related to this Monitored Service,
+then the Pager doesn’t notify any Target
+and doesn’t set an acknowledgement delay
+```
+
+```
+Given a Monitored Service in an Unhealthy State,
+when the Pager receives a Healthy event related to this Monitored Service
+and later receives the Acknowledgement Timeout,
+then the Monitored Service becomes Healthy,
+the Pager doesn’t notify any Target
+and doesn’t set an acknowledgement delay
+```
---
+++ models.py
@@ -0,0 +1,105 @@
+import datetime
+from typing import List, Optional
+
+
+from abc import ABC, abstractmethod
+
+class Target(ABC):
+    @abstractmethod
+    def notify(self, message: str):
+        pass
+
+class Email(Target):
+    def __init__(self, email_address):
+        self.email_address = email_address
+    
+    def notify(self, message: str) -> str:
+        res = f'Emailing {self.email_address}: {message}'
+        # TODO: send email adapter here
+        return res
+    
+class SMS(Target):
+    def __init__(self, phone_number):
+        self.phone_number = phone_number
+    
+    def notify(self, message: str) -> str:
+        res = f'Sending SMS to {self.phone_number}: {message}'
+        # TODO: send sms adapter here
+        return res
+
+class MonitoredService:
+    def __init__(self, service_name):
+        self.service_name = service_name
+        self.healthy = True
+    
+    def set_unhealthy(self):
+        self.healthy = False
+    
+    def set_healthy(self):
+        self.healthy = True
+
+class EscalationPolicyLevel:
+    def __init__(self, targets: List[Target]):
+        self.targets = targets
+
+class EscalationPolicyMonitoredService:
+    def __init__(self, monitored_service: MonitoredService, levels: List[EscalationPolicyLevel]):
+        self.monitored_service = monitored_service
+        self.levels: List[EscalationPolicyLevel] = levels
+
+    def level_targets(self, level: int) -> List[Target]:
+        return self.levels[level].targets
+
+class EscalationPolicy:
+    def __init__(self, policies: dict):
+        self.policies: dict = policies # Dictionary with monitored_service name as keys and EscalationPolicyMonitoredService as values
+
+class Timer:
+    def __init__(self, created_at: datetime.datetime):
+        self.created_at: datetime.datetime = created_at
+        self.timeout: Optional[datetime.datetime] = self.created_at + datetime.timedelta(minutes=15)
+        self.acknowledged: bool = False
+    
+    def acknowledge(self):
+        self.acknowledged = True
+        self.timeout = None
+
+class Alert:
+    def __init__(self, monitored_service):
+        self.monitored_service: MonitoredService = monitored_service
+        self.sent_at: datetime.datetime = datetime.datetime.now()
+        self.current_level: int = 0
+        self.message: str = f'{monitored_service.service_name} is unhealthy (Level {self.current_level})'
+        self.timer = Timer(self.sent_at)
+    
+    def escalate(self):
+        self.current_level += 1
+
+    def acknowledge(self):
+        self.timer.acknowledge()
+    
+    def __str__(self):
+        return (
+            f"Alert for {self.monitored_service.service_name}\n"
+            f"Sent at {self.sent_at}\n"
+            f"Current escalation level: {self.current_level}\n"
+            f"Timeout for acknowledge: {self.timer.timeout}\n"
+            f"Acknowledged: {self.timer.acknowledged}"
+        )
+
+class PagerService:
+    def __init__(self, escalation_policy: EscalationPolicy):
+        self.escalation_policy: EscalationPolicy = escalation_policy
+        self.alerts: List[Alert] = []
+        self.alerts_log = [] # log 
+    
+    def receive_alert(self, alert: Alert):
+        # append the alert to the list of alerts
+        self.alerts.append(alert)
+        # set the service to unhealthy
+        alert.monitored_service.set_unhealthy()
+        # send the alert to all targets of the escalation policy current level
+        targets = self.escalation_policy.policies[alert.monitored_service.service_name].levels[alert.current_level].targets
+        for target in targets:
+            self.alerts_log.append(target.notify(alert.message))
+        # sets timer acknowledgment delay to 15 minutes
